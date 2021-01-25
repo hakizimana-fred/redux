0e6c2e081ecefabf3295fdd2eb2dc770350b3c04
hakizimana-fred <hakifred20@gmail.com>
2021-01-25 21:27:10 +0300

    listening to changes

--- index.js
+++ index.js
@@ -3,11 +3,26 @@
 function createStore() {
 
     let state
+    let listeners = []
 
+    const subscribe = (listener) => {
+        listeners.push(listener)
+    }
     const getState = () => state
 
     return {
-        state
+        getState,
+        subscribe
     }
 
 }
+
+const store = createStore()
+
+store.subscribe(() => {
+    console.log('The state is ', store.getState())
+})
+
+store.subscribe(() => {
+    console.log('The store changed')
+})
