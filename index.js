9b944f0c27024db26abe832b19dde770696b9d55
hakizimana-fred <hakifred20@gmail.com>
2021-01-25 22:33:28 +0300

    goals reducer

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ function todos(state = [], action) {
 
 }
 
+function goals(state = [], action) {
+
+    switch (action.type) {
+        case 'ADD_GOAL':
+            return state.concat([action.goal])
+        case 'REMOVE_GOAL':
+            return state.filter(goal => goal.id !== action.id)
+        default:
+            return state
+
+    }
+}
 
 // creating a store
 const store = createStore(todos)
